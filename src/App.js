1e6075b36446e6fb6cdca421c02140e515df22d8
Mac Rowe <m.rowe42@gmail.com>
2021-09-14 14:22:08 -0400

    Use navigator and geolocation to create first useEffect hook

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ function App() {
   const[ lat, setLat ] = useState([]);
   const [ long, setLong ] = useState([]);
 
+  useEffect( () => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    console.log("Latitude is: ", lat)
+    console.log("Longitude is: ", long)
+  }, [lat, long]);
+
   return (
     <div className="App">
 
