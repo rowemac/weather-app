debb22f5f0fe5f86313a5c3d27ce3adbcc261edd
Mac Rowe <m.rowe42@gmail.com>
2021-09-14 14:42:55 -0400

    Add fetch from API

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
+// import Weather from './components/weather';
+
 
 function App() {
 
   const[ lat, setLat ] = useState([]);
   const [ long, setLong ] = useState([]);
+  const [ data, setData ] = useState([]);
 
-  useEffect( () => {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    });
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
 
-    console.log("Latitude is: ", lat)
-    console.log("Longitude is: ", long)
-  }, [lat, long]);
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+    }
+    fetchData();
+  }, [lat,long])
 
   return (
     <div className="App">
