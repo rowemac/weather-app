887490b7d2418e5c9a531342f49dae3c3821dc01
Mac Rowe <m.rowe42@gmail.com>
2021-09-15 11:24:17 -0400

    Add sunrise/sunset to card

--- src/components/weather.js
+++ src/components/weather.js
@@ -6,12 +6,19 @@ const CardExampleCard = ( { weatherData} ) => (
     <div className="main">
         <p className="header"> { weatherData.name } </p>
         
-        <div>
-            <p className="day">Day: { moment().format('dddd') } </p>
+        <div className="flex">
+            <p className="day">{moment().format('dddd')}, <span>{moment().format('LL')}</span></p>
+            <p className="description">{ weatherData.weather[0].main }</p>
         </div>
 
-        <div>
+        <div className="flex">
             <p className="temp">Temperature: { weatherData.main.temp } &deg;C</p>
+            <p className="temp">Humidity: { weatherData.main.humidity } %</p>
+        </div>
+
+        <div className="flex">
+        <p className="sunrise-sunset">Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+        <p className="sunrise-sunset">Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
         </div>
     </div>
 )
